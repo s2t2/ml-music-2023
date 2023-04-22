ad04b8687c752f41addeb8da0d695443cc16dbf4
MJ Rossetti <s2t2@users.noreply.github.com>
2023-04-22 16:22:56 -0400

    Implement dropout and regularization

--- app/jobs/train_gtzan_nn.py
+++ app/jobs/train_gtzan_nn.py
@@ -15,6 +15,7 @@ from category_encoders import OneHotEncoder #, OrdinalEncoder
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Dropout
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l2
 #from tensorflow.keras.callbacks import EarlyStopping
 
 import plotly.express as px
@@ -23,7 +24,13 @@ from app import GTZAN_DIRPATH
 
 
 N_MFCC = int(os.getenv("N_MFCC", default=20))
+
+TEST_SIZE = float(os.getenv("TEST_SIZE", default=0.3))
+
 N_EPOCHS = int(os.getenv("N_EPOCHS", default=50))
+LAMBDA = float(os.getenv("LAMBDA", default=0.001))
+DROPOUT_RATE = float(os.getenv("DROPOUT_RATE", default=0.1))
+LEARNING_RATE = float(os.getenv("LEARNING_RATE", default=0.0001))
 
 
 def load_gtzan_mfcc(n_mfcc, encode_labels=True):
@@ -60,12 +67,69 @@ def load_gtzan_mfcc(n_mfcc, encode_labels=True):
     return x, y
 
 
+def train_model(x_train, y_train, x_test, y_test,
+                #layer_sizes=[512, 256, 64],
+                l2_lambda=LAMBDA, dropout_rate=DROPOUT_RATE,
+                learning_rate=LEARNING_RATE, n_epochs=N_EPOCHS
+    ):
+
+    model = Sequential([
+        # input layer:
+        # ... x shape is (n_tracks, 1292, 20), so we are specifying the shape of the individual mfcc features:
+        Flatten(input_shape=(x.shape[1], x.shape[2])),
+
+        # dense layers:
+        Dense(512, activation="relu", kernel_regularizer=l2(l2_lambda)),
+        Dropout(dropout_rate, seed=99),
+
+        Dense(256, activation="relu", kernel_regularizer=l2(l2_lambda)),
+        Dropout(dropout_rate, seed=99),
+
+        Dense(64, activation="relu", kernel_regularizer=l2(l2_lambda)),
+        Dropout(dropout_rate, seed=99),
+
+        # output layer:
+        Dense(10, activation="softmax")
+    ])
+
+    # compile model:
+    model.compile(
+        optimizer=Adam(learning_rate=learning_rate),
+        loss="categorical_crossentropy", # use with one-hot encoded labels
+        metrics=["accuracy"]
+    )
+    model.summary()
+
+    # train model:
+    history = model.fit(x_train, y_train, batch_size=32, epochs=n_epochs,
+        validation_data=(x_test, y_test),
+        #callbacks=[EarlyStopping(monitor="loss", patience=10)]
+    )
+
+    # plot results:
+    history_df = DataFrame({
+        "train_accy": history.history["accuracy"],
+        "val_accy": history.history["val_accuracy"]
+    })
+    history_df["epoch"] = history_df.index + 1
+
+    title = f"GTZAN Genre Classifier (Neural Net) <br><sup>Params: test_size={TEST_SIZE}, dropout_rate={dropout_rate}, l2_lambda={l2_lambda}, learning_rate={learning_rate}</sup>"
+    fig = px.line(history_df, x="epoch", y=["train_accy", "val_accy"], title=title)
+    fig.show()
+
+    ## Evaluate the model on the test set
+    #test_err, test_acc = model.evaluate(x_test, y_test)
+    #print("ACCY TEST:", test_acc)
+    #print("ERR TEST:", test_err)
+
+
 if __name__ == "__main__":
 
     x, y = load_gtzan_mfcc(n_mfcc=N_MFCC)
+
     print("-----------------------")
     print("TRAIN TEST SPLIT...")
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=99)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SIZE, random_state=99)
     print("X TRAIN:", x_train.shape)
     print("Y TRAIN:", y_train.shape)
     print("X TEST:", x_test.shape)
@@ -73,44 +137,4 @@ if __name__ == "__main__":
 
     print("-----------------------")
     print("TRAINING NEURAL NETWORK FOR GENRE CLASSIFICATION...")
-
-    model = Sequential([
-        # input layer:
-        # ... x shape is (n_tracks, 1292, 20), so we are specifying the shape of the individual mfcc features:
-        Flatten(input_shape=(x.shape[1], x.shape[2])),
-        # dense layers:
-        Dense(512, activation="relu"),
-        Dense(256, activation="relu"),
-        Dense(64, activation="relu"),
-        # output layer:
-        Dense(10, activation="softmax")
-    ])
-
-    # compile model:
-    model.compile(
-        optimizer=Adam(learning_rate=0.0001),
-        loss="categorical_crossentropy", # use with one-hot encoded labels
-        metrics=["accuracy"]
-    )
-    model.summary()
-
-    # train model:
-    history = model.fit(x_train, y_train, batch_size=32, epochs=N_EPOCHS,
-        validation_data=(x_test, y_test),
-        #callbacks=[EarlyStopping(monitor="loss", patience=5)]
-    )
-
-    history_df = DataFrame({
-        "epoch": range(1, N_EPOCHS+1),
-        "train_accy": history.history["accuracy"],
-        "val_accy": history.history["val_accuracy"]
-    })
-    fig = px.line(history_df, x="epoch", y=["train_accy", "val_accy"],
-        title="GTZAN Genre Classifier (Neural Net)",
-    )
-    fig.show()
-
-    ## Evaluate the model on the test set
-    #test_err, test_acc = model.evaluate(x_test, y_test)
-    #print("ACCY TEST:", test_acc)
-    #print("ERR TEST:", test_err)
+    train_model(x_train, y_train, x_test, y_test)
