68a4f0080946bc4b9fcde83c6f6062d1d8538b25
MJ Rossetti <s2t2@users.noreply.github.com>
2023-05-21 16:02:31 -0400

    WIP - working with separated audio, this is very memory intensive

--- app/audio_splitter.py
+++ app/audio_splitter.py
@@ -2,19 +2,24 @@ import os
 
 
 from spleeter.separator import Separator
-from spleeter.audio.adapter import AudioAdapter
+#from spleeter.audio.adapter import AudioAdapter
 import numpy as np
+from pandas import DataFrame
 
 from app.audio_processor import AudioProcessor
 
 
 N_STEMS = int(os.getenv("N_STEMS", default="2"))
 
+STEMS_MAP = {
+    2: ["vocals", "other"],
+    4: ["vocals", "drums", "bass", "other"],
+    5: ["vocals", "piano", "drums", "bass", "other"],
+}
+
 
 class AudioSplitter(AudioProcessor):
-    def __init__(self, audio_filepath, #, output_filepath=None,
-                 #n_stems=N_STEMS
-                ):
+    def __init__(self, audio_filepath):
         """
         Params :
 
@@ -43,3 +48,8 @@ class AudioSplitter(AudioProcessor):
         model_name = f"spleeter:{n_stems}stems"
         sep = Separator(model_name)
         return sep.separate(audio_data.reshape(-1, 1))
+
+    #def split_df(self, n_stems=N_STEMS, audio_data=None):
+    #    splits = self.split(n_stems, audio_data)
+    #    breakpoint()
+    #    return DataFrame(splits)
---
+++ app/jobs/split_youtube_audio.py
@@ -0,0 +1,74 @@
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+from app.youtube_dataset import YoutubeDataset #, AudioFile as YoutubeAudioFile
+
+from app.audio_processor import TRACK_LENGTH
+from app.audio_splitter import AudioSplitter, N_STEMS, STEMS_MAP
+
+import numpy as np
+from pandas import DataFrame
+
+
+if __name__ == "__main__":
+
+    print("-----------")
+    ds = YoutubeDataset()
+    print(f"YOUTUBE AUDIO FILES ({len(ds.audio_files)})...")
+    print(f"ARTISTS ({len(ds.artist_names)}):", sorted(ds.artist_names))
+
+    print("-----------")
+    print(f"STEMS ({N_STEMS}):", STEMS_MAP[N_STEMS])
+
+    results = []
+    for audio_file in ds.audio_files[0:1]:
+        print(audio_file.audio_filename)
+
+        ap = AudioSplitter(audio_file.audio_filepath)
+        tracks = ap.tracks(track_length_seconds=TRACK_LENGTH)
+        for i, track in enumerate(tracks):
+            track = np.array(track)
+            print("TRACK", i, track.shape)
+
+            #mfcc = ap.mfcc(n_mfcc=N_MFCC, audio_data=track)
+            #mfcc = mfcc.T
+            ##print(audio_filename, track.shape, mfcc.shape)
+
+            result = {
+                "audio_filename": audio_file.audio_filename,
+                "track_number": i+1,
+                "track_length": len(track),
+                #"mfcc_rows": mfcc.shape[0], # related to the track length
+                #"mfcc_cols": mfcc.shape[1], # should equal n_mfcc
+                #"mfcc": mfcc,
+            }
+
+            print("SPLITTING...")
+            splits = ap.split(n_stems=N_STEMS, audio_data=track)
+            for stem_name, stem_vals in splits.items():
+                # https://github.com/deezer/spleeter/issues/849
+                # https://github.com/deezer/spleeter/wiki/4.-API-Reference#raw-waveform-based-separation
+                # each stem has two columns, which have similar values
+                # could take the first or the second, or average
+                # to represent each stem as a single column
+                result[stem_name] = stem_vals.mean(axis=1)
+
+            results.append(result)
+
+
+
+
+
+
+        #vocals_df = DataFrame(results)
+
+        breakpoint()
+        #
+        # RUNNING INTO ERROR:
+        #
+        # self.pid = util.spawnv_passfds(spawn.get_executable(),
+        # File "/opt/anaconda3/envs/ml-music/lib/python3.10/multiprocessing/util.py", line 450, in spawnv_passfds
+        #     errpipe_read, errpipe_write = os.pipe()
+        # OSError: [Errno 24] Too many open files
